f30e0733b0c0f931ee548cda9110cef26d85f244
midochan44 <54564976+midochan44@users.noreply.github.com>
2020-02-18 17:34:44 +0900

    Update FooterJ.js

--- src/components/Japanese/FooterJ.js
+++ src/components/Japanese/FooterJ.js
@@ -7,9 +7,9 @@ export default class FooterJ extends React.Component {
         <div className="text">
           <p>みどり翻訳サービス</p>
 
-          <p>〒 152-0035</p>
+          <p>〒 202-0005</p>
 
-          <p>東京都目黒区自由が丘3-2-17-202</p>
+          <p>東京都西東京市住吉町３−１２−１６</p>
 
           <p>TEL: 042-439-6159</p>
           <p>FAX: 042-439-4903</p>
