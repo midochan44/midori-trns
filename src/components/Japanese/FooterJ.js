e145c10f4596a06334a499aa731888ae212d2181
midochan44 <54564976+midochan44@users.noreply.github.com>
2020-03-11 19:55:32 -0700

    Update FooterJ.js

--- src/components/Japanese/FooterJ.js
+++ src/components/Japanese/FooterJ.js
@@ -7,7 +7,7 @@ export default class FooterJ extends React.Component {
         <div className="text">
           <p>みどり翻訳サービス</p>
           <p>mail: midoritrns@gmail.com</p>
-          <p>TEL: 042-439-6159</P>
+          <p>TEL: 042-439-6159</p>
           <p>FAX: 042-439-4903</p>
           <p>住所は自宅のため必要に応じてお伝えしています。</p>
           
